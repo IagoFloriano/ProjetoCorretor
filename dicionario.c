59978d4c87117061835f660955c8a5a1ba236b89
IagoFloriano <iagomfloriano@gmail.com>
2020-08-24 20:48:35 -0300

    Tentativa de implementar funcao para ordenar o dicionario

--- dicionario.c
+++ dicionario.c
@@ -1,6 +1,12 @@
 #include "dicionario.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#ifndef MAXCHARSINWORD
+#define MAXCHARSINWORD 100
+#endif
 
 int readDict(char ***dict, char *path) {
   // Abre arquivo e testa se teve sucesso em abrir o arquivo
@@ -12,11 +18,13 @@ int readDict(char ***dict, char *path) {
   *dict = malloc(sizeof(char *) * ALLOCSIZE); // Alocar espaco para os ponteiros das strings no dicionario
   int dictAllocs = 1;                         // Variavel para guardar quantas vezes for alocado espaco para linhas no dicionario
 
-  const int wordSize = 100; //define como 100 o tamanho maximo de cada palavra
-  int currentLine = 0;      // Variavel para marcar onde deve ser salva cada palavra
+  int currentLine = 0; // Variavel para marcar onde deve ser salva cada palavra
 
-  (*dict)[0] = malloc(sizeof(char) * wordSize);
-  while (fgets((*dict)[currentLine], wordSize, dictFile)) {
+  (*dict)[0] = malloc(sizeof(char) * MAXCHARSINWORD);
+  while (fgets((*dict)[currentLine], MAXCHARSINWORD, dictFile)) {
+    // Remove o '\n' no final da palavra
+    (*dict)[currentLine][strcspn((*dict)[currentLine], "\n")] = '\0';
+
     currentLine++;
     // Alocar mais linhas para o dicionario caso precise
     if (currentLine >= dictAllocs * ALLOCSIZE) {
@@ -24,9 +32,34 @@ int readDict(char ***dict, char *path) {
       *dict = realloc(*dict, sizeof(char *) * dictAllocs * ALLOCSIZE);
     }
     // Alocagem de espaco para a linha a ser lida na proxima passagem do loop
-    (*dict)[currentLine] = malloc(sizeof(char) * wordSize);
+    (*dict)[currentLine] = malloc(sizeof(char) * MAXCHARSINWORD);
   }
 
   fclose(dictFile);
   return currentLine;
+}
+
+void dictToLower(char ***dict, int size) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; (*dict)[i][j] != '\0'; j++) {
+      (*dict)[i][j] = tolower((*dict)[i][j]);
+    }
+  }
+}
+
+int stringcompare(const void *a, const void *b) {
+  const char *pa = *(const char **)a;
+  const char *pb = *(const char **)b;
+  return (strcmp(pa, pb));
+}
+
+void dictSort(char ***dict, int size) {
+  qsort((*dict)[0], size, sizeof(char *), stringcompare);
+}
+
+// Funcao para visualizar o dicionario
+void printDict(char **dict, int size) {
+  for (int i = 0; i < size; i++) {
+    printf("Line: %d Word: %s\n", i, dict[i]);
+  }
 }
--- dicionario.h
+++ dicionario.h
@@ -7,3 +7,12 @@
 // Le dicionario que esta salvo em path
 // Funcao assume que a variavel "dict" ainda nao tem espaco alocado
 int readDict(char ***dict, char *path);
+
+// Funcao para deixar todas as palavras do dicionario apenas com letras minusculas
+void dictToLower(char ***dict, int size);
+
+// Funcao para ordenar o dicionario
+void dictSort(char ***dict, int size);
+
+// Funcao para visualizar o dicionario
+void printDict(char **dict, int size);
